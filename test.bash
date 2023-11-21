b6ae4eefb31282e9a31365e46ccd632e48718cd5
Yuma Takimoto <motsucha549@gmail.com>
2023-11-21 13:07:51 +0900

    Add License

--- test.bash
+++ test.bash
@@ -1,25 +1,16 @@
 #!/bin/bash
-# NG関数：エラーメッセージを表示し、結果を1に設定
-ng() {
-    echo "NG at Line $1"
-    res=1
-}
 
-# テストケースの配列
-declare -a tests=("1\n2\n3\n4\n5" "1.5\n2.5\n3.5\n4.5\n5.5" "1\n2.5\n3\n4.5\n5")
-# 期待される出力の配列
-declare -a expected=("1\n3\n6\n10\n15\nFinal total: 15" "1.5\n4.0\n7.5\n12.0\n17.5\nFinal total: 17.5" "1\n3.5\n6.5\n11.0\n16.0\nFinal total: 16.0")
+# テストケース：整数のリスト
+echo -e "1\n2\n3\n4\n5" | ./sum_input.py > output.txt
+diff output.txt <(echo -e "1\n3\n6\n10\n15\nFinal total: 15") || echo "Test 1 failed"
 
-# 結果の初期化
-res=0
+# テストケース：浮動小数点数のリスト
+echo -e "1.5\n2.5\n3.5\n4.5\n5.5" | ./sum_input.py > output.txt
+diff output.txt <(echo -e "1.5\n4.0\n7.5\n12.0\n17.5\nFinal total: 17.5") || echo "Test 2 failed"
 
-# 各テストケースをループで処理
-for i in "${!tests[@]}"; do
-    echo -e "${tests[$i]}" | ./sum_input.py > output.txt
-    diff output.txt <(echo -e "${expected[$i]}") || ng $((i+1))
-done
+# テストケース：整数と浮動小数点数の混在
+echo -e "1\n2.5\n3\n4.5\n5" | ./sum_input.py > output.txt
+diff output.txt <(echo -e "1\n3.5\n6.5\n11.0\n16.0\nFinal total: 16.0") || echo "Test 3 failed"
 
-# 結果の確認と表示
-[ "$res" = 0 ] && echo "OK"
+echo "All tests passed!"
 rm output.txt
-exit $res
