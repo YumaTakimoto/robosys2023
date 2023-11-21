8d12b3ad8da684764d311551369d788b3dcf037f
Yuma Takimoto <motsucha549@gmail.com>
2023-11-21 17:11:23 +0900

    Add License

--- test.bash
+++ test.bash
@@ -6,16 +6,16 @@ ng () {
 }
 
 # テストケース：整数のリスト
-echo -e "1\n2\n3\n4\n5" | ./plus.py > output.txt
-diff output.txt <(echo -e "1\n3\n6\n10\n15\nFinal total: 15") || ng 1
+out=$(echo -e "1\n2\n3\n4\n5" | ./plus)
+[ "${out}" = "1\n3\n6\n10\n15\nFinal total: 15" ] || ng ${LINENO}
 
 # テストケース：浮動小数点数のリスト
-echo -e "1.5\n2.5\n3.5\n4.5\n5.5" | ./plus.py > output.txt
-diff output.txt <(echo -e "1.5\n4.0\n7.5\n12.0\n17.5\nFinal total: 17.5") || ng 2
+out=$(echo -e "1.5\n2.5\n3.5\n4.5\n5.5" | ./plus)
+[ "${out}" = "1.5\n4.0\n7.5\n12.0\n17.5\nFinal total: 17.5" ] || ng ${LINENO}
 
 # テストケース：整数と浮動小数点数の混在
-echo -e "1\n2.5\n3\n4.5\n5" | ./plus.py > output.txt
-diff output.txt <(echo -e "1\n3.5\n6.5\n11.0\n16.0\nFinal total: 16.0") || ng 3
+out=$(echo -e "1\n2.5\n3\n4.5\n5" | ./plus)
+[ "${out}" = "1\n3.5\n6.5\n11.0\n16.0\nFinal total: 16.0" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo "OK"
 exit $res
